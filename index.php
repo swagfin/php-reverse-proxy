756a09cd4a16e048ea012b09cc37c9bd1f52dcb2
Wainaina George (Swagfin) <39550012+swagfin@users.noreply.github.com>
2021-08-13 02:17:49 +0300

    Updated from no php

--- index.php
+++ index.php
@@ -1,125 +1,7 @@
-<?php
-class ProxyHandler
-{
-    private $url;
-    private $translated_url;
-    private $curl_handler;
-
-    function __construct($url, $proxy_url)
-    {
-        $this->url = $url;
-        $this->proxy_url = $proxy_url;
-
-        // Parse all the parameters for the URL
-        if (isset($_SERVER['PATH_INFO']))
-        {
-            $proxy_url .= $_SERVER['PATH_INFO'];
-        }
-        else
-        {
-            $proxy_url .= '/';
-        }
-
-        if ($_SERVER['QUERY_STRING'] !== '')
-        {
-            $proxy_url .= "?{$_SERVER['QUERY_STRING']}";
-        }
-
-        $this->translated_url = $proxy_url;
-
-        $this->curl_handler = curl_init($proxy_url);
-
-        // Set various options
-        $this->setCurlOption(CURLOPT_RETURNTRANSFER, true);
-        $this->setCurlOption(CURLOPT_BINARYTRANSFER, true); // For images, etc.
-		if(isset($_SERVER['HTTP_USER_AGENT'])){	$this->setCurlOption(CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);}
-		$this->setCurlOption(CURLOPT_WRITEFUNCTION, array(
-            $this,
-            'readResponse'
-        ));
-        $this->setCurlOption(CURLOPT_HEADERFUNCTION, array(
-            $this,
-            'readHeaders'
-        ));
-
-        // Process post data.
-        if (count($_POST))
-        {
-            // Empty the post data
-            $post = array();
-
-            // Set the post data
-            $this->setCurlOption(CURLOPT_POST, true);
-
-            // Encode and form the post data
-            foreach ($_POST as $key => $value)
-            {
-                $post[] = urlencode($key) . "=" . urlencode($value);
-            }
-
-            $this->setCurlOption(CURLOPT_POSTFIELDS, implode('&', $post));
-
-            unset($post);
-        }
-        elseif ($_SERVER['REQUEST_METHOD'] !== 'GET') // Default request method is 'get'
-        
-        {
-            // Set the request method
-            $this->setCurlOption(CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
-        }
-
-    }
-
-    // Executes the proxy.
-    public function execute()
-    {
-        curl_exec($this->curl_handler);
-    }
-
-    // Get the information about the request.
-    // Should not be called before exec.
-    public function getCurlInfo()
-    {
-        return curl_getinfo($this->curl_handler);
-    }
-
-    // Sets a curl option.
-    public function setCurlOption($option, $value)
-    {
-        curl_setopt($this->curl_handler, $option, $value);
-    }
-
-    protected function readHeaders(&$cu, $string)
-    {
-        $length = strlen($string);
-        if (preg_match(',^Location:,', $string))
-        {
-            $string = str_replace($this->proxy_url, $this->url, $string);
-        }
-        header($string);
-        return $length;
-    }
-
-    protected function readResponse(&$cu, $string)
-    {
-        $length = strlen($string);
-        echo $string;
-        return $length;
-    }
-
+<?PHP
 /*
-the >>>>> .htaccess file:
-
-RewriteEngine On
-RewriteCond %{REQUEST_FILENAME} !-f [NC]
-RewriteCond %{REQUEST_FILENAME} !-d [NC]
-RewriteCond %{REQUEST_URI} !^/index.php
-RewriteRule ^(.+)$ index.php/$1 [QSA]
-*/
-}
-//USAGE
-$host = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_ENV['FORCE_HTTPS']) && $_ENV['FORCE_HTTPS'] == 'true')) ? 'https' : 'http';
-$host .= '://' . $_SERVER['HTTP_HOST']."/";
+ * HAVE YOUR OWN ENDPOINTS
+ */
 //Available Servers
 $servers = array(
 				'http://localhost:9091/',
@@ -129,7 +11,138 @@ $servers = array(
 				);
 $proxyTo = $servers[array_rand($servers)];
 //exit("Proxxied to: $proxyTo");
-//Proceed
-$proxy = new ProxyHandler($host,$proxyTo);
-$proxy->execute();
+
+$backend_url = $proxyTo;
+$backend_info = parse_url($backend_url);
+$host = $_SERVER['HTTP_HOST'];
+$request_uri = $_SERVER['REQUEST_URI'];
+$uri_rel = "subdir/index.php"; # URI to this file relative to public_html
+
+$request_includes_nophp_uri = true;
+if ( $request_includes_nophp_uri == false) {
+    $request_uri = str_replace( $uri_rel, "/", $request_uri );
+}
+
+$is_ruby_on_rails = false;
+if ( $is_ruby_on_rails == true) {
+    # You have to understand the Ruby on Rails Asset pipeline to understand this.
+    $request_uri = str_replace( "$uri_rel/assets", "/assets", $request_uri );
+}
+
+$url = $backend_url . $request_uri;
+
+
+function getRequestHeaders($multipart_delimiter=NULL) {
+    $headers = array();
+    foreach($_SERVER as $key => $value) {
+        if(preg_match("/^HTTP/", $key)) { # only keep HTTP headers
+            if(preg_match("/^HTTP_HOST/", $key) == 0 && # let curl set the actual host/proxy
+            preg_match("/^HTTP_ORIGIN/", $key) == 0 &&
+            preg_match("/^HTTP_CONTENT_LEN/", $key) == 0 && # let curl set the actual content length
+            preg_match("/^HTTPS/", $key) == 0
+            ) {
+                $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($key, 5)))));
+                if ($key)
+                    array_push($headers, "$key: $value");
+            }
+        } elseif (preg_match("/^CONTENT_TYPE/", $key)) {
+
+            $key = "Content-Type";
+
+            if(preg_match("/^multipart/", strtolower($value)) && $multipart_delimiter) {
+                $value = "multipart/form-data; boundary=" . $multipart_delimiter;
+                array_push($headers, "$key: $value");
+            }
+            else if(preg_match("/^application\/json/", strtolower($value))) {
+                // Handle application/json
+                array_push($headers, "$key: $value");
+            }
+        }
+    }
+    return $headers;
+}
+
+function build_domain_regex($hostname)
+{
+	$names = explode('.', $hostname); //assumes main domain is the TLD
+	$regex = "";
+	for ($i= 0; $i < count ($names)-2; $i++)
+	{
+		$regex .= '['.$names[$i].'.]?';
+	}
+	$main_domain = $names[count($names)-2] .".". $names[count($names)-1];
+	$regex .= $main_domain;
+	return $regex;
+}
+  
+function build_multipart_data_files($delimiter, $fields, $files) {
+    # Inspiration from: https://gist.github.com/maxivak/18fcac476a2f4ea02e5f80b303811d5f :)
+    $data = '';
+    $eol = "\r\n";
+  
+    foreach ($fields as $name => $content) {
+        $data .= "--" . $delimiter . $eol
+            . 'Content-Disposition: form-data; name="' . $name . "\"".$eol.$eol
+            . $content . $eol;
+    }
+  
+    foreach ($files as $name => $content) {
+        $data .= "--" . $delimiter . $eol
+            . 'Content-Disposition: form-data; name="' . $name . '"; filename="' . $name . '"' . $eol
+            . 'Content-Transfer-Encoding: binary'.$eol
+            ;
+        $data .= $eol;
+        $data .= $content . $eol;
+    }
+    $data .= "--" . $delimiter . "--".$eol;
+
+    return $data;
+}
+
+$curl = curl_init( $url );
+curl_setopt( $curl, CURLOPT_HTTPHEADER, getRequestHeaders() );
+curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true ); # follow redirects
+curl_setopt( $curl, CURLOPT_HEADER, true ); # include the headers in the output
+curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true ); # return output as string
+
+if ( strtolower($_SERVER['REQUEST_METHOD']) == 'post' ) {
+    curl_setopt( $curl, CURLOPT_POST, true );
+    $post_data = file_get_contents("php://input");
+
+    if (preg_match("/^multipart/", strtolower($_SERVER['CONTENT_TYPE']))) {
+        $delimiter = '-------------' . uniqid();
+        $post_data = build_multipart_data_files($delimiter, $_POST, $_FILES);
+        curl_setopt( $curl, CURLOPT_HTTPHEADER, getRequestHeaders($delimiter) );
+    }
+
+    curl_setopt( $curl, CURLOPT_POSTFIELDS, $post_data );
+}
+  
+$contents = curl_exec( $curl ); # reverse proxy. the actual request to the backend server.
+curl_close( $curl ); # curl is done now
+
+
+$contents = preg_replace('/^HTTP\/1.1 3.*(?=HTTP\/1\.1)/sm', '', $contents); # remove redirection headers
+list( $header_text, $contents ) = preg_split( '/([\r\n][\r\n])\\1/', $contents, 2 );
+
+$headers_arr = preg_split( '/[\r\n]+/', $header_text ); 
+  
+// Propagate headers to response.
+foreach ( $headers_arr as $header ) {
+    if ( !preg_match( '/^Transfer-Encoding:/i', $header ) ) {
+        if ( preg_match( '/^Location:/i', $header ) ) {
+            # rewrite absolute local redirects to relative ones
+            $header = str_replace($backend_url, "/", $header);
+        }
+        else if ( preg_match( '/^set-cookie:/i', $header ) ) {
+			# replace original domain name in Set-Cookie headers with our server's domain
+			$domain_regex = build_domain_regex($backend_info['host']);
+			$header = preg_replace('/Domain='.$domain_regex.'/', 'Domain='.$host, $header);
+		}
+        header( $header, false );
+    }
+}
+
+print $contents; # return the proxied request result to the browser
+
 ?>
