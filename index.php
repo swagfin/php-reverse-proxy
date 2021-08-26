024e40f7b2236f4f1cf544b53a282be7d4778f20
Wainaina George (Swagfin) <39550012+swagfin@users.noreply.github.com>
2021-08-26 03:26:43 +0300

    Added Partitioned Based Load Balancing

--- index.php
+++ index.php
@@ -4,11 +4,19 @@
  */
 //Available Servers
 $servers = array(
-				"http://localhost:9091/",
-				"http://localhost:9092/",
-				);
+	"http://localhost:9091/",
+	"http://localhost:9092/",
+	);
+
+//#FOR RANDOM (Randomized Load Balancer)
 $proxyTo = $servers[array_rand($servers)];
 //exit("Proxxied to: $proxyTo");
+
+//#FOR PARTITION BASED (Using IP Address as Partition Key)
+//$partitionKey = $_SERVER['HTTP_HOST'];
+//$proxyTo = getPartitionRoute($partitionKey, $servers);
+
+
 
 $backend_url = $proxyTo;
 $backend_info = parse_url($backend_url);
@@ -18,49 +26,65 @@ $uri_rel = "subdir/index.php"; # URI to this file relative to public_html
 
 $request_includes_nophp_uri = true;
 if ( $request_includes_nophp_uri == false) {
-    $request_uri = str_replace( $uri_rel, "/", $request_uri );
+	$request_uri = str_replace( $uri_rel, "/", $request_uri );
 }
 
 $is_ruby_on_rails = false;
 if ( $is_ruby_on_rails == true) {
     # You have to understand the Ruby on Rails Asset pipeline to understand this.
-    $request_uri = str_replace( "$uri_rel/assets", "/assets", $request_uri );
+	$request_uri = str_replace( "$uri_rel/assets", "/assets", $request_uri );
 }
 
 $url = $backend_url . $request_uri;
 
+//New Feature
+function getPartitionRoute($key, $routes)
+{
+	$string = md5($key);
+	$bytes = array();
+	for($i = 0; $i < strlen($string); $i++){
+		$bytes[] = ord($string[$i]);
+	}
 
+	$maxPartitions =count($routes);
+	$firstByte = $bytes[0];
+	$unMappedPartitionNr = $firstByte;
+	$unMappedPartitionNr = abs($unMappedPartitionNr);
+	$partitionIndex = $unMappedPartitionNr % $maxPartitions;
+//Response
+	return $routes[$partitionIndex];
+}
 function getRequestHeaders($multipart_delimiter=NULL) {
-    $headers = array();
-    foreach($_SERVER as $key => $value) {
+	$headers = array();
+	foreach($_SERVER as $key => $value) {
         if(preg_match("/^HTTP/", $key)) { # only keep HTTP headers
             if(preg_match("/^HTTP_HOST/", $key) == 0 && # let curl set the actual host/proxy
-            preg_match("/^HTTP_ORIGIN/", $key) == 0 &&
+            	preg_match("/^HTTP_ORIGIN/", $key) == 0 &&
             preg_match("/^HTTP_CONTENT_LEN/", $key) == 0 && # let curl set the actual content length
             preg_match("/^HTTPS/", $key) == 0
             ) {
-                $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($key, 5)))));
-                if ($key)
-                    array_push($headers, "$key: $value");
+            	$key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($key, 5)))));
+            	if ($key)
+            		array_push($headers, "$key: $value");
             }
         } elseif (preg_match("/^CONTENT_TYPE/", $key)) {
 
-            $key = "Content-Type";
+        	$key = "Content-Type";
 
-            if(preg_match("/^multipart/", strtolower($value)) && $multipart_delimiter) {
-                $value = "multipart/form-data; boundary=" . $multipart_delimiter;
-                array_push($headers, "$key: $value");
-            }
-            else if(preg_match("/^application\/json/", strtolower($value))) {
+        	if(preg_match("/^multipart/", strtolower($value)) && $multipart_delimiter) {
+        		$value = "multipart/form-data; boundary=" . $multipart_delimiter;
+        		array_push($headers, "$key: $value");
+        	}
+        	else if(preg_match("/^application\/json/", strtolower($value))) {
                 // Handle application/json
-                array_push($headers, "$key: $value");
-            }
+        		array_push($headers, "$key: $value");
+        	}
         }
     }
 	//Append Header IP
-	$remoteAddress = $_SERVER["REMOTE_ADDR"];
-	array_push($headers, "remoteAddress: $remoteAddress");
-	
+    $remoteAddress = $_SERVER["REMOTE_ADDR"];
+    array_push($headers, "remoteAddress: $remoteAddress");
+
     return $headers;
 }
 
@@ -76,29 +100,29 @@ function build_domain_regex($hostname)
 	$regex .= $main_domain;
 	return $regex;
 }
-  
+
 function build_multipart_data_files($delimiter, $fields, $files) {
     # Inspiration from: https://gist.github.com/maxivak/18fcac476a2f4ea02e5f80b303811d5f :)
-    $data = '';
-    $eol = "\r\n";
-  
-    foreach ($fields as $name => $content) {
-        $data .= "--" . $delimiter . $eol
-            . 'Content-Disposition: form-data; name="' . $name . "\"".$eol.$eol
-            . $content . $eol;
-    }
-  
-    foreach ($files as $name => $content) {
-        $data .= "--" . $delimiter . $eol
-            . 'Content-Disposition: form-data; name="' . $name . '"; filename="' . $name . '"' . $eol
-            . 'Content-Transfer-Encoding: binary'.$eol
-            ;
-        $data .= $eol;
-        $data .= $content . $eol;
-    }
-    $data .= "--" . $delimiter . "--".$eol;
+	$data = '';
+	$eol = "\r\n";
 
-    return $data;
+	foreach ($fields as $name => $content) {
+		$data .= "--" . $delimiter . $eol
+		. 'Content-Disposition: form-data; name="' . $name . "\"".$eol.$eol
+		. $content . $eol;
+	}
+
+	foreach ($files as $name => $content) {
+		$data .= "--" . $delimiter . $eol
+		. 'Content-Disposition: form-data; name="' . $name . '"; filename="' . $name . '"' . $eol
+		. 'Content-Transfer-Encoding: binary'.$eol
+		;
+		$data .= $eol;
+		$data .= $content . $eol;
+	}
+	$data .= "--" . $delimiter . "--".$eol;
+
+	return $data;
 }
 
 $curl = curl_init( $url );
@@ -108,18 +132,18 @@ curl_setopt( $curl, CURLOPT_HEADER, true ); # include the headers in the output
 curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true ); # return output as string
 
 if ( strtolower($_SERVER['REQUEST_METHOD']) == 'post' ) {
-    curl_setopt( $curl, CURLOPT_POST, true );
-    $post_data = file_get_contents("php://input");
+	curl_setopt( $curl, CURLOPT_POST, true );
+	$post_data = file_get_contents("php://input");
 
-    if (preg_match("/^multipart/", strtolower($_SERVER['CONTENT_TYPE']))) {
-        $delimiter = '-------------' . uniqid();
-        $post_data = build_multipart_data_files($delimiter, $_POST, $_FILES);
-        curl_setopt( $curl, CURLOPT_HTTPHEADER, getRequestHeaders($delimiter) );
-    }
+	if (preg_match("/^multipart/", strtolower($_SERVER['CONTENT_TYPE']))) {
+		$delimiter = '-------------' . uniqid();
+		$post_data = build_multipart_data_files($delimiter, $_POST, $_FILES);
+		curl_setopt( $curl, CURLOPT_HTTPHEADER, getRequestHeaders($delimiter) );
+	}
 
-    curl_setopt( $curl, CURLOPT_POSTFIELDS, $post_data );
+	curl_setopt( $curl, CURLOPT_POSTFIELDS, $post_data );
 }
-  
+
 $contents = curl_exec( $curl ); # reverse proxy. the actual request to the backend server.
 curl_close( $curl ); # curl is done now
 
@@ -128,21 +152,21 @@ $contents = preg_replace('/^HTTP\/1.1 3.*(?=HTTP\/1\.1)/sm', '', $contents); # r
 list( $header_text, $contents ) = preg_split( '/([\r\n][\r\n])\\1/', $contents, 2 );
 
 $headers_arr = preg_split( '/[\r\n]+/', $header_text ); 
-  
+
 // Propagate headers to response.
 foreach ( $headers_arr as $header ) {
-    if ( !preg_match( '/^Transfer-Encoding:/i', $header ) ) {
-        if ( preg_match( '/^Location:/i', $header ) ) {
+	if ( !preg_match( '/^Transfer-Encoding:/i', $header ) ) {
+		if ( preg_match( '/^Location:/i', $header ) ) {
             # rewrite absolute local redirects to relative ones
-            $header = str_replace($backend_url, "/", $header);
-        }
-        else if ( preg_match( '/^set-cookie:/i', $header ) ) {
+			$header = str_replace($backend_url, "/", $header);
+		}
+		else if ( preg_match( '/^set-cookie:/i', $header ) ) {
 			# replace original domain name in Set-Cookie headers with our server's domain
 			$domain_regex = build_domain_regex($backend_info['host']);
 			$header = preg_replace('/Domain='.$domain_regex.'/', 'Domain='.$host, $header);
 		}
-        header( $header, false );
-    }
+		header( $header, false );
+	}
 }
 
 print $contents; # return the proxied request result to the browser
